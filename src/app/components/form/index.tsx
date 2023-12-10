f645fd86836516e6c4e8e32ec5f036ec82b6ac2a
Felipe Santos <dev.felipesantos@gmail.com>
2023-12-10 15:55:58 -0300

    feat: create inputs logic

--- src/app/components/form/index.tsx
+++ src/app/components/form/index.tsx
@@ -1,45 +1,65 @@
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  Card,
-} from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+'use client'
+
+import { useEffect, useState } from 'react'
+import { CardTitle, CardDescription, CardHeader, CardContent, Card } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export default function FormComplete() {
+  const [code, setCode] = useState('')
+  const [verified, setVerified] = useState(false)
+
+  const verifyCode = () => {
+    if (code === '1234') {
+      setVerified(true)
+    } else {
+      setVerified(false)
+    }
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card className="space-y-4">
         <CardHeader>
-          <CardTitle>Verification Form</CardTitle>
-          <CardDescription>
-            Please enter your details for verification.
-          </CardDescription>
+          <CardTitle className={`text-center`}>Vale Cortesia AnimeGeek 2024</CardTitle>
+          <CardDescription className={`text-center`}>Preencha os dados a seguir</CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
           <div className="space-y-2">
-            <Label htmlFor="initial-input">Initial Input</Label>
-            <Input id="initial-input" placeholder="Enter your input" />
-            <Button className="w-full" type="submit">
-              Verify
+            <Label htmlFor="initial-input">Código de cortesia</Label>
+            <Input
+              id="initial-input"
+              placeholder="Preencha o código de cortesia"
+              value={code}
+              onChange={e => setCode(e.target.value)}
+            />
+            <Button className="w-full" onClick={verifyCode}>
+              Verificar
             </Button>
           </div>
-          <div className="border-t pt-4 space-y-2">
-            <Label htmlFor="second-input">Second Input</Label>
-            <Input id="second-input" placeholder="Enter your input" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="third-input">Third Input</Label>
-            <Input id="third-input" placeholder="Enter your input" />
-            <Button className="w-full" type="submit">
-              Submit
-            </Button>
-          </div>
+
+          {verified && (
+            <>
+              <div className="border-t pt-4 space-y-2">
+                <Label htmlFor="second-input">Nome Completo</Label>
+                <Input id="second-input" placeholder="Preencha com seu nome completo" />
+              </div>
+              <div className="border-t pt-4 space-y-2">
+                <Label htmlFor="second-input">E-mail</Label>
+                <Input id="second-input" placeholder="Preencha com um e-mail válido" />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="third-input">Contato whatsapp</Label>
+                <Input id="third-input" placeholder="Preencha com seu número de whatsapp" />
+                <Button className="w-full mt-2" type="submit">
+                  Enviar
+                </Button>
+              </div>
+            </>
+          )}
         </CardContent>
       </Card>
     </div>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
-import FormComplete from "./components/form";
+import FormComplete from './components/form'
 
 export default function Home() {
-  return <FormComplete />;
+  return (
+    <div className={`flex-col`}>
+      <div className={`flex-1 space-y-4 p-8 pt-20`}>
+        <FormComplete />
+      </div>
+    </div>
+  )
 }
